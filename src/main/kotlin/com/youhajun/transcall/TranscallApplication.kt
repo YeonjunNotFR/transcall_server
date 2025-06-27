77f66ef68c150363fe28af418b6a594e62ce2b91
yeonjun <kgbduswns@gmail.com>
2025-06-27 12:00:00 +0900

    feat: docker-compose .env 파일에서 환경변수 읽어오도록

--- src/main/kotlin/com/youhajun/transcall/TranscallApplication.kt
+++ src/main/kotlin/com/youhajun/transcall/TranscallApplication.kt
@@ -2,8 +2,10 @@ package com.youhajun.transcall
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing
 
 @SpringBootApplication
+@EnableR2dbcAuditing
 class TranscallApplication
 
 fun main(args: Array<String>) {

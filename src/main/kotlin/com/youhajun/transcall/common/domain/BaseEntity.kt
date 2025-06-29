9b7f40bfc3eeb09af3f0ce619ceb4e33f535d217
yeonjun <kgbduswns@gmail.com>
2025-06-29 12:00:00 +0900

    feat: common 패키지 BaseEntity 생성

---
+++ src/main/kotlin/com/youhajun/transcall/common/domain/BaseEntity.kt
@@ -0,0 +1,14 @@
+package com.youhajun.transcall.common.domain
+
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import java.time.LocalDateTime
+
+abstract class BaseEntity(
+
+    @CreatedDate
+    var createdAt: LocalDateTime? = null,
+
+    @LastModifiedDate
+    var updatedAt: LocalDateTime? = null
+)

f185e577960dd0a876bb84643c7a5ac3ceb01874
yeonjun <kgbduswns@gmail.com>
2025-07-01 12:00:00 +0900

    feat: User 패키지 UserException 생성

---
+++ src/main/kotlin/com/youhajun/transcall/user/exception/UserException.kt
@@ -0,0 +1,8 @@
+package com.youhajun.transcall.user.exception
+
+import com.youhajun.transcall.common.exception.ErrorType
+import com.youhajun.transcall.common.exception.TransCallException
+
+sealed class UserException(errorType: ErrorType): TransCallException(errorType) {
+    class UserNotFoundException : UserException(ErrorType.USER_NOT_FOUND)
+}

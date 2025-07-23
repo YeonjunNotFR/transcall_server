38c1a2f4dcc8fc948349df1d8b94eec3b60dbd7b
YeonjunKim <44648608+YeonjunNotPed@users.noreply.github.com>
2025-07-23 12:00:00 +0900

    feat: config, Flyway 설정 추가 및 application.yml 수정

---
+++ src/main/kotlin/com/youhajun/transcall/FlywayConfig.kt
@@ -0,0 +1,27 @@
+package com.youhajun.transcall
+
+import org.flywaydb.core.Flyway
+import org.springframework.boot.autoconfigure.flyway.FlywayProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class FlywayConfig {
+
+    @Bean
+    fun flyway(flywayProperties: FlywayProperties): Flyway {
+        val flyway = Flyway.configure()
+            .baselineOnMigrate(flywayProperties.isBaselineOnMigrate)
+            .baselineVersion(flywayProperties.baselineVersion)
+            .dataSource(flywayProperties.url, flywayProperties.user, flywayProperties.password)
+            .load()
+
+        flyway.migrate()
+        return flyway
+    }
+
+    @Bean
+    fun flywayProperties(): FlywayProperties {
+        return FlywayProperties()
+    }
+}

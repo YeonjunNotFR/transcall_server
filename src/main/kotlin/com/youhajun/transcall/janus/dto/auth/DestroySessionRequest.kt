920632a39cbd401a3ee60a329d817ec826bf875a
YeonjunKim <44648608+YeonjunNotPed@users.noreply.github.com>
2025-08-31 12:00:00 +0900

    feat: janus/auth, CreateSessionRequest 및 CreateSessionResponse, DestroySessionRequest 추가

---
+++ src/main/kotlin/com/youhajun/transcall/janus/dto/auth/DestroySessionRequest.kt
@@ -0,0 +1,12 @@
+package com.youhajun.transcall.janus.dto.auth
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.youhajun.transcall.janus.dto.BaseJanusRequest
+import com.youhajun.transcall.janus.dto.JanusCommand
+
+data class DestroySessionRequest(
+    @JsonProperty("session_id")
+    val sessionId: Long
+) : BaseJanusRequest() {
+    override val janus: JanusCommand = JanusCommand.DESTROY
+}

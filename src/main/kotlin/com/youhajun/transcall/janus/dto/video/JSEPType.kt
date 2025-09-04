80227ee758d6cd0208d730ddb6b1774154d27136
YeonjunKim <44648608+YeonjunNotPed@users.noreply.github.com>
2025-09-04 12:00:00 +0900

    feat: janus/video, JSEPType 및 PeerType 데이터 클래스 추가

---
+++ src/main/kotlin/com/youhajun/transcall/janus/dto/video/JSEPType.kt
@@ -0,0 +1,18 @@
+package com.youhajun.transcall.janus.dto.video
+
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonValue
+
+enum class JSEPType(val type: String) {
+    OFFER("offer"),
+    ANSWER("answer");
+
+    @JsonValue
+    override fun toString(): String = type
+
+    companion object {
+        @JvmStatic
+        @JsonCreator
+        fun fromType(type: String): JSEPType? = entries.firstOrNull { it.type == type.lowercase() }
+    }
+}
---
+++ src/main/kotlin/com/youhajun/transcall/janus/dto/video/PeerType.kt
@@ -0,0 +1,18 @@
+package com.youhajun.transcall.janus.dto.video
+
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonValue
+
+enum class PeerType(val type: String) {
+    PUBLISHER("publisher"),
+    SUBSCRIBER("subscriber");
+
+    @JsonValue
+    override fun toString(): String = type
+
+    companion object {
+        @JvmStatic
+        @JsonCreator
+        fun fromType(type: String): PeerType? = entries.firstOrNull { it.type == type.lowercase() }
+    }
+}

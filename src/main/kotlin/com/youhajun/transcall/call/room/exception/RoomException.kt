84f66e77a8b61dde5082e05bde631684b7139015
YeonjunKim <44648608+YeonjunNotPed@users.noreply.github.com>
2025-09-18 12:00:00 +0900

    feat: room, RoomIsFull 예외 추가

--- src/main/kotlin/com/youhajun/transcall/call/room/exception/RoomException.kt
+++ src/main/kotlin/com/youhajun/transcall/call/room/exception/RoomException.kt
@@ -6,4 +6,5 @@ import com.youhajun.transcall.common.exception.TransCallException
 sealed class RoomException(errorType: ErrorType) : TransCallException(errorType) {
     class RoomCodeGenerationFailed : RoomException(ErrorType.ROOM_CODE_GENERATION_FAILED)
     class RoomNotFound : RoomException(ErrorType.ROOM_NOT_FOUND)
+    class RoomIsFull : RoomException(ErrorType.ROOM_IS_FULL)
 }

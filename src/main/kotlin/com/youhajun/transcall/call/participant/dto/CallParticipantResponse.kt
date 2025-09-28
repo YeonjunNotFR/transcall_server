8db3031ff5cb56ecb18c5b7dae56d01453e37c3b
YeonjunKim <44648608+YeonjunNotPed@users.noreply.github.com>
2025-09-28 12:00:00 +0900

    feat: feature:participant, CallParticipantResponse 에 userId, languageCode, countryCode 필드 추가

--- src/main/kotlin/com/youhajun/transcall/call/participant/dto/CallParticipantResponse.kt
+++ src/main/kotlin/com/youhajun/transcall/call/participant/dto/CallParticipantResponse.kt
@@ -1,10 +1,13 @@
 package com.youhajun.transcall.call.participant.dto
 
+import com.youhajun.transcall.user.domain.CountryType
 import com.youhajun.transcall.user.domain.LanguageType
 
 data class CallParticipantResponse(
     val participantId: String,
+    val userId: String,
     val displayName: String,
     val profileImageUrl: String,
-    val language: LanguageType,
+    val languageCode: LanguageType,
+    val countryCode: CountryType
 )

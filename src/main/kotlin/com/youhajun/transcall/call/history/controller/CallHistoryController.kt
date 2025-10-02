a3bcb70d6b6ddccdcb63fb5d356161b5638dfac3
YeonjunKim <44648608+YeonjunNotPed@users.noreply.github.com>
2025-10-02 12:00:00 +0900

    feat: history controller, 통화 기록 상세 조회 API 추가
    통화 기록 상세 조회 API를 추가하여 특정 통화 기록을 조회할 수 있도록 합니다.
    historyId를 통해 특정 통화 기록을 조회합니다.
    
    주요 변경 사항:
    * 통화 기록 상세 조회 API 추가
    * historyId를 통해 특정 통화 기록을 조회

--- src/main/kotlin/com/youhajun/transcall/call/history/controller/CallHistoryController.kt
+++ src/main/kotlin/com/youhajun/transcall/call/history/controller/CallHistoryController.kt
@@ -1,16 +1,16 @@
 package com.youhajun.transcall.call.history.controller
 
-import com.youhajun.transcall.common.domain.UserPrincipal
 import com.youhajun.transcall.call.history.dto.CallHistoryResponse
 import com.youhajun.transcall.call.history.service.CallHistoryService
+import com.youhajun.transcall.common.domain.UserPrincipal
 import com.youhajun.transcall.pagination.dto.CursorPage
+import com.youhajun.transcall.pagination.vo.CursorPagination
 import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotBlank
 import org.springframework.security.core.Authentication
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
 @RequestMapping("/api/call")
@@ -25,6 +25,17 @@ class CallHistoryController(
         @Min(1) @RequestParam(defaultValue = "30") first: Int,
     ): CursorPage<CallHistoryResponse> {
         val principal = authentication.principal as UserPrincipal
-        return callHistoryService.getCallHistories(principal.userId, after, first)
+        val pagination = CursorPagination(after, first)
+        return callHistoryService.getCallHistories(principal.userId, pagination)
+    }
+
+    @GetMapping("history/{historyId}")
+    suspend fun getCallHistory(
+        authentication: Authentication,
+        @NotBlank @PathVariable historyId: String,
+    ): CallHistoryResponse {
+        val principal = authentication.principal as UserPrincipal
+        val historyUUID = UUID.fromString(historyId)
+        return callHistoryService.getCallHistory(principal.userId, historyUUID)
     }
 }

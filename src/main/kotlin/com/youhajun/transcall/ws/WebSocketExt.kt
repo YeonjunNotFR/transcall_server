5198a2f9e970cd78a5a4e556be13ce3fefd2d464
YeonjunKim <44648608+YeonjunNotPed@users.noreply.github.com>
2025-10-10 12:00:00 +0900

    feat: ws, WebSocket 확장 함수 추가
    WebSocket 세션 내에서 서버 메시지 전송 및 바이너리 메시지 전송을
    위한 확장 함수를 추가하여, 코드 재사용성을 높이고 메시지 전송 로직을
    캡슐화합니다.
    
    주요 변경 사항:
    - WebSocketSession.sendServerMessage 함수 추가
    - WebSocketSession.sendBinaryMessage 함수 추가

---
+++ src/main/kotlin/com/youhajun/transcall/ws/WebSocketExt.kt
@@ -0,0 +1,26 @@
+package com.youhajun.transcall.ws
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.youhajun.transcall.ws.dto.ServerMessage
+import kotlinx.coroutines.reactive.awaitFirstOrNull
+import kotlinx.coroutines.reactor.awaitSingleOrNull
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import org.springframework.web.reactive.socket.WebSocketSession
+import reactor.core.publisher.Mono
+
+private val logger: Logger = LogManager.getLogger("WebSocketExt")
+
+suspend fun WebSocketSession.sendServerMessage(serverMessage: ServerMessage, objectMapper: ObjectMapper) {
+    if(!isOpen) { logger.warn("Socket is closed"); return }
+    val payload = objectMapper.writeValueAsString(serverMessage)
+    send(Mono.just(textMessage(payload))).awaitSingleOrNull()?.also {
+        logger.info("Sending Server Message: {}", payload)
+    }
+}
+
+suspend fun WebSocketSession.sendBinaryMessage(message: ByteArray) {
+    if(!isOpen) { logger.warn("Socket is closed"); return }
+    val wsMsg = binaryMessage { factory -> factory.wrap(message) }
+    send(Mono.just(wsMsg)).awaitFirstOrNull()
+}

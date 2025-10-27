b16ba260e32772a327d9c495d29bd1615dc91131
YeonjunKim <44648608+YeonjunNotPed@users.noreply.github.com>
2025-10-27 12:00:00 +0900

    refactor: data:room Janus 비디오룸 DTO를 sealed interface로 통합
    기존에 분산되어 있던 Janus 비디오룸 응답 DTO들을 단일 sealed interface로 통합하여 코드의 응집도와 유지보수성을 높입니다. 다양한 비디오룸 이벤트 및 응답을 타입 안전하게 처리하고 관리하기 용이해집니다.
    
    주요 변경 사항:
    - `VideoRoomResponse` sealed interface를 생성하고 관련 DTO들을 통합
    - `CreateVideoRoomResponse`, `JanusPublisherResponse` 등 개별 DTO 파일 삭제
    - `CreateVideoRoomResponse`를 `CreateRoomResponse`로 리네임하여 재구성

--- src/main/kotlin/com/youhajun/transcall/janus/dto/video/response/CreateRoomResponse.kt
+++ src/main/kotlin/com/youhajun/transcall/janus/dto/video/response/CreateRoomResponse.kt
@@ -2,9 +2,9 @@ package com.youhajun.transcall.janus.dto.video.response
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
-data class CreateVideoRoomResponse(
-    @JsonProperty("room")
-    val janusRoomId: Long,
+data class CreateRoomResponse(
+    @JsonProperty("room") val
+    janusRoomId: Long,
 ) {
     @JsonProperty("videoroom")
     val videoRoom: VideoRoomResponseType = VideoRoomResponseType.CREATED
